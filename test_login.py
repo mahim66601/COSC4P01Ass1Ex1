d3642f014662f0d121748c40a05b669ac21b0388
mahim66601 <mahiimm66601@gmail.com>
2024-10-06 21:27:39 -0400

    rename test class and file

---
+++ test_login.py
@@ -0,0 +1,18 @@
+import login
+
+class TestLogin:
+    def test_empty(self):
+        "Test empty username and password "
+        assert login.login('', '') == False
+    def test_success(self):
+        "Test correct username and password"
+        assert login.login('admin', 'admin') == True
+    def test_fail(self):
+        "Test username password case sensitivity"
+        assert login.login('ADmin', 'adMin') == False
+    def test_multiple_attempts(self):
+        "Test lockout after three failed attempts"
+        login.login('admin', 'test')
+        login.login('admin', 'test')
+        login.login('admin', 'test')
+        assert login.login('admin', 'admin') == False
